c6196535b82bf12e5000948f6bed5e5de6bc716e
tim <rosstimo@isu.edu>
2023-02-21 08:37:04 -0700

    add simple example

--- ListsConsoleApp/ListExamples.vb
+++ ListsConsoleApp/ListExamples.vb
@@ -5,6 +5,27 @@ Imports System
 
 Module ListExamples
     Sub Main(args As String())
+        SimpleList()
 
     End Sub
+
+    Sub SimpleList()
+        'constructor for new list 
+        Dim names As New List(Of String)
+
+        names.Add("Bill")
+        names.Add("Mary")
+        names.Add("Joe")
+        names.Add("Jill")
+        names.Add("Sally")
+
+        Console.WriteLine($"The list contains: {names.Count} items")
+        Console.WriteLine($"Does the list contain Frank? {names.Contains("Frank}")}")
+        Console.WriteLine($"Mary is at index: {names.IndexOf("Mary")}")
+        Console.WriteLine($"The name at index 1 is: {names.Item(1)}")
+        Console.WriteLine($"The last item in the list is: {names.Last}")
+
+    End Sub
+
+
 End Module
